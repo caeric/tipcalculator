dabc25aad4231591bf561b0cffb69883da1bf926
erhung <erhung@yahoo-inc.com>
2014-01-02 14:02:31 -0800

    Initial Commit

---
+++ tipcalculator/erhAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  erhAppDelegate.h
+//  tipcalculator
+//
+//  Created by Eric Hung on 1/2/14.
+//  Copyright (c) 2014 Eric Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface erhAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Eric Hung on 1/2/14.
+//  Copyright (c) 2014 Eric Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "erhAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([erhAppDelegate class]));
+    }
+}
